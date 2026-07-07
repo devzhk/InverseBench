8e3008bb4906bd54aa6b34a0bea59916e4a3b3fc
devzhk <devzhk@gmail.com>
2026-07-07 03:36:35 +0000

    Fix DMPlug inference logging

--- algo/dmplug.py
+++ algo/dmplug.py
@@ -1,23 +1,16 @@
-import copy
-import json
-import os
-
 import torch
 from tqdm import tqdm
-import torch.nn.functional as F
-from torchvision.utils import save_image
 from .base import Algo
 from utils.scheduler import Scheduler
 from utils.diffusion import DiffusionSampler
 
 
 class DMPlug(Algo):
-    
     '''
     DMPlug algorithm implemented in EDM framework.
     '''
-    
-    def __init__(self, 
+
+    def __init__(self,
                  net,
                  forward_op,
                  diffusion_scheduler_config,
@@ -30,7 +23,7 @@ class DMPlug(Algo):
                  solver='euler',
                  ):
         super(DMPlug, self).__init__(net, forward_op)
-        self.net.eval().requires_grad_(False)
+        self.net.eval()
         self.diffusion_scheduler_config = diffusion_scheduler_config
         self.scheduler = Scheduler(**diffusion_scheduler_config)
         self.guidance_scale = guidance_scale
@@ -63,11 +56,8 @@ class DMPlug(Algo):
         if data is not None:
             return numel(data)
         raise TypeError("Cannot infer measurement size for loss_scaling='mse'.")
-        
+
     def inference(self, observation, num_samples=1, **kwargs):
-        target = kwargs.get('target')
-        evaluator = kwargs.get('evaluator')
-        trace_dir = kwargs.get('trace_dir')
         device = self.forward_op.device
         if num_samples > 1:
             if not torch.is_tensor(observation):
@@ -75,12 +65,12 @@ class DMPlug(Algo):
             observation = observation.repeat(num_samples, *([1] * (observation.ndim - 1)))
         x_initial = torch.randn(num_samples, self.net.img_channels, self.net.img_resolution, self.net.img_resolution, device=device) * self.scheduler.sigma_max
         x_initial.requires_grad = True
-        
+
         sampler = DiffusionSampler(self.scheduler, solver=self.solver)
         pbar = tqdm(range(self.iteration))
-        
+
         optimizer = torch.optim.AdamW([x_initial], lr=self.lr, weight_decay=self.weight_decay)
-        
+
         for iteration in pbar:
             optimizer.zero_grad()
             denoised = sampler.sample(self.net, x_initial, SDE=self.sde, verbose=False)
@@ -96,9 +86,15 @@ class DMPlug(Algo):
                 x_initial_grad = x_initial_grad * 0.5 / torch.sqrt(loss_scale).clamp_min(1e-8)
             elif self.loss_scaling == 'mse':
                 x_initial_grad = x_initial_grad / self._measurement_numel(observation)
-            x_initial.grad = x_initial_grad * self.guidance_scale
-            desc = f'Iteration {iteration + 1}/{self.iteration}. Data fitting loss: {display_loss}, x_initial.grad norm: {grad_norm}'
-            
+            x_initial.grad = (x_initial_grad * self.guidance_scale).detach()
+            display_loss = torch.sqrt(loss_scale.detach()).item()
+            grad_norm = x_initial.grad.detach().norm().item()
+            desc = (
+                f'Iteration {iteration + 1}/{self.iteration}. '
+                f'Data fitting loss: {display_loss:.4f}, '
+                f'x_initial.grad norm: {grad_norm:.4f}'
+            )
+
             optimizer.step()
             pbar.set_description(desc)
 
